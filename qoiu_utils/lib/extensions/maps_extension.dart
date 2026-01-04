604603a3f6d5985b1846b1cae72a464cafbacb03
qoiu <guschinnick@gmail.com>
2026-01-04 11:27:34 +0300

    add maps_extension.dart

--- qoiu_utils/lib/extensions/maps_extension.dart
+++ qoiu_utils/lib/extensions/maps_extension.dart
@@ -1,3 +1,3 @@
 extension MapsExtension<K extends dynamic, V extends dynamic> on Map<K, V> {
-  V? nullAt(K key) => containsKey(key) ? this[key] : null;
+  V? nullAt(K key,[V? value]) => containsKey(key) ? this[key] : value;
 }
