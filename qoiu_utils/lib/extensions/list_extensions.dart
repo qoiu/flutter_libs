2c700ceb38c2da59a46efe2aef32c504ee04d114
qoiu <guschinnick@gmail.com>
2026-02-20 11:32:47 +0300

    fix list_extensions.dart

--- qoiu_utils/lib/extensions/list_extensions.dart
+++ qoiu_utils/lib/extensions/list_extensions.dart
@@ -1,4 +1,8 @@
 
+import 'dart:math';
+
 extension ListExtension<T> on List<T>{
   List<R> indexedMap<R>(R Function(int index,T data) mapper)=>indexed.map((e)=>mapper(e.$1,e.$2)).toList();
+
+  T random()=>this[Random().nextInt(length)];
 }
