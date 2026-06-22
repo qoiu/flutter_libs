e9e2aecead4cf35f9c1b6a6943b3b05e1a2cf2b2
qoiu <guschinnick@gmail.com>
2026-06-22 10:03:34 +0300

    add oppositeTextColor

--- qoiu_utils/lib/extensions/color.dart
+++ qoiu_utils/lib/extensions/color.dart
@@ -3,4 +3,9 @@ import 'package:flutter/material.dart';
 
 extension ColorFilterOnColor on Color {
   ColorFilter defaultFilter() => ColorFilter.mode(this, BlendMode.srcATop);
+
+  Color oppositeTextColor() {
+    return computeLuminance() > 0.5 ? Colors.black : Colors.white;
+  }
+
 }
